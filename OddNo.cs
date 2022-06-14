335cc48d722c1141d76dda354cd39c0a528aae89
Shubhalisonar <shubhali97@gmail.com>
2022-06-14 12:01:42 +0530

    Add project files.

---
+++ OddNo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofttechTranningAss
+{
+    internal class OddNo
+    {
+        public static void main1()
+        {
+            Console.WriteLine("Odd numbers from 1 to 99. Prints one number per line.");
+            for (int n = 1; n < (99 + 1); n++)
+            {
+                if (n % 2 != 0)
+                {
+                    Console.WriteLine(n.ToString());
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofttechTranningAss
+{
+    internal class Program
+    {
+        static void main2(string[] args)
+        {
+        }
+    }
+}
